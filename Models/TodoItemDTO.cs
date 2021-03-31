0e1ac54730f85dec946ee5b3d69e7b4645ab3776
Danu Mahendra <80309128+danudevphoton@users.noreply.github.com>
2021-03-31 11:00:24 +0700

    init

---
+++ Models/TodoItemDTO.cs
@@ -0,0 +1,9 @@
+namespace C_Web_Api.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
